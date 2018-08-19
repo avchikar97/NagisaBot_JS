bfb72e8705c7404eef67cdf797bb075adb2ef01c
Akaash Chikarmane <avchikar97@gmail.com>
2018-08-19 18:55:29 -0500

    Converted to discord.js

--- index.js
+++ index.js
@@ -1,12 +1,44 @@
-const Discord = require('discord.js');
-const bot = new Discord.Client();
-
-bot.on('message', (message) => {
-    if(message.content == 'ping'){
-        //message.reply('pong');
-        message.channel.sendMessage('pong');
-    }
-
+/* const commando = require('discord.js-commando');
+const config = require('./config.json');
+const bot = new commando.Client({
+    commandPrefix : config.prefix,
+    owner: '224949031514800128',
+    disableEveryone : true,
+    unknownCommandResponse: true
 });
+
+
+bot.registry.registerGroup('random', 'Random');
+bot.registry.registerGroup('welcome', 'Welcome');
+bot.registry.registerGroup('test', 'Test');
+bot.registry.registerDefaults();
+bot.registry.registerCommandsIn(__dirname + "/commands");
+
+bot.on('ready', () => {
+    console.log('Logged in!');
+    bot.user.setActivity('Dango Daikazoku');
+}); */
+
+const config = require('./config.json');
+const Discord = require('discord.js');
+
+const bot = new Discord.Client({
+    disableEveryone: true
+});
+
+bot.on("ready", async() => {
+    console.log(bot.user.username + " is online!");
+    bot.user.setActivity("Dango Daikazoku");
+});
+
+bot.on("message", async message => {
+    if(message.author.bot) return;
+    if(message.channel.type === "dm") return;
+
+    let prefix = config.prefix;
+    let messageArray = message.content.split(" ");
+    let cmd = messageArray[0];
+    let args = messageArray.slice(1);
+})
 
 bot.login(config.token);
